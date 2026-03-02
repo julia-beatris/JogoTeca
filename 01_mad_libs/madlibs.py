10cc44c64d03ea677e17ebece01fed9a5ca8c8c3
julia beatris <julia.b.santos10@edu.senai.br>
2026-03-02 17:08:32 -0300

    ADD: Criado o jogo mad libs

---
+++ 01_mad_libs/madlibs.py
@@ -0,0 +1,31 @@
+print('''
+                       _   _ _ _         
+                     | | | (_) |        
+  _ __ ___   __ _  __| | | |_| |__  ___ 
+ | '_ ` _ \ / _` |/ _` | | | | '_ \/ __|
+ | | | | | | (_| | (_| | | | | |_) \__ \ 
+ |_| |_| |_|\__,_|\__,_| |_|_|_.__/|___/                                                                                                       
+                              ,--.     ,--.        ,--.                ,--.                              ,--.                          
+,--.,--.,--,--,--. ,--,--.    |  ,---. `--' ,---.,-'  '-. ,---. ,--.--.`--' ,--,--.    ,--,--,--. ,--,--.|  |,--.,--. ,---. ,--,--.    
+|  ||  ||        |' ,-.  |    |  .-.  |,--.(  .-''-.  .-'| .-. ||  .--',--.' ,-.  |    |        |' ,-.  ||  ||  ||  || .--'' ,-.  |    
+'  ''  '|  |  |  |\ '-'  |    |  | |  ||  |.-'  `) |  |  ' '-' '|  |   |  |\ '-'  |    |  |  |  |\ '-'  ||  |'  ''  '\ `--.\ '-'  |    
+ `----' `--`--`--' `--`--'    `--' `--'`--'`----'  `--'   `---' `--'   `--' `--`--'    `--`--`--' `--`--'`--' `----'  `---' `--`--'    
+                                                                                                                                    
+''')
+verbo=input("me fale um verbo:")
+animal=input("me fale um animal:")
+adjetivo=input("me fale um adjetivo:")
+objeto=input("me fale um objeto:")
+lugar=input("me fale um lugar:")
+emoção=input("me fale um emoção:")
+
+print (f'''
+    Era uma vez um(a) pequeno(a) {animal} que vivia em um(a) {lugar} muito especial. 
+    Ele(a) era conhecido(a) por ser extremamente {adjetivo} e adorava {verbo} sempre que tinha oportunidade.
+    Todas as manhãs, acordava cedo, pegava seu(sua) {objeto} favorito(a) e saía para explorar os arredores. 
+    Enquanto caminhava, imaginava grandes aventuras e sonhava em descobrir algo incrível.
+    Certo dia, enquanto tentava {verbo} perto de uma parte misteriosa do(a) {lugar}, encontrou algo inesperado. 
+    Com seu(sua) {objeto} nas mãos e o coração acelerado, precisou ser muito {adjetivo} para continuar.
+    No final dessa aventura, percebeu que o mais importante não era o que havia encontrado, mas a(o) {emoção} que sentiu durante toda a jornada.
+    E assim, voltou para casa mais sábio(a), pronto(a) para novas histórias.
+''' )
