d1eae3ec8193d3033ef9699a63a3020cf06b2c46
julia beatris <julia.b.santos10@edu.senai.br>
2026-03-30 17:02:41 -0300

    ADD:TRABALHEI COM NOVOS JOGOS PARA A NOSSA JOGO TECA

---
+++ 05_pedra_papel_tesoura/pedra_papel_tesoura.py
@@ -0,0 +1,36 @@
+import random 
+print("""\033[35m
+            _           _                   _                _                            
+  __ _ __ _| |_ ___    | |__  __ _ _ _ __ _| |_ __ _   ___  | |_ _  _ _ __  __ _ _ _  ___ 
+ / _` / _` |  _/ _ \_  | '_ \/ _` | '_/ _` |  _/ _` | / -_) | ' \ || | '  \/ _` | ' \/ _ -
+ \__, \__,_|\__\___( ) |_.__/\__,_|_| \__,_|\__\__,_| \___| |_||_\_,_|_|_|_\__,_|_||_\___/
+ |___/             |/                                                                     
+      """)
+
+
+escolha_computador= random.choice (["gato","barata","humano"])
+escolha_jogador= input("você escolhe gato,barata ou humano?")
+print(f'Eu escolhi {escolha_computador}!')
+if escolha_jogador == "gato":
+    if escolha_computador == "gato":
+        print('empate')
+    elif escolha_computador == 'barata':
+        print('parabens você venceu')
+    else:
+        print('você perdeu hahah')
+
+if escolha_jogador == "barata":
+    if escolha_computador == "barata":
+        print('empate')
+    elif escolha_computador == 'humano':
+        print('parabens você venceu')
+    else:
+        print('você perdeu hahaha')
+
+if escolha_jogador == "humano":
+    if escolha_computador == "humano":
+        print('empate')
+    elif escolha_computador == 'gato':
+        print('parabens você venceu')
+    else:
+        print('você perdeu hahaha')
---
+++ 06-cara_coroa/cara_coroa.py
@@ -0,0 +1,34 @@
+import random
+
+print("""                                           
+  ___ __ _ _ __ __ _    ___  _   _    ___ ___  _ __ ___   __ _ 
+ / __/ _` | '__/ _` |  / _ \| | | |  / __/ _ \| '__/ _ \ / _` |
+| (_| (_| | | | (_| | | (_) | |_| | | (_| (_) | | | (_) | (_| |
+ \___\__,_|_|  \__,_|  \___/ \__,_|  \___\___/|_|  \___/ \__,_|                        
+  
+      
+                         
+                     ..;;;--'~~~`--;;;..
+                    /;-~IN GOD WE TRUST~-.\
+                   //      ,;;;;;;;;      \\
+                 .//      ;;;;;    \       \\
+                 ||       ;;;;(   /.|       ||
+                 ||       ;;;;;;;   _\      ||
+                 ||       ';;  ;;;;=        ||
+                ||LIBERTY | ''\;;;;;;      ||
+                  \\     ,| '\  '|><| 1995 //
+                   \\   |     |      \  A //
+                    `;.,|.    |      '\.-'/
+                      ~~;;;,._|___.,-;;;~'
+                        ''=--<dcau>'
+
+                                                               
+      """)
+
+escolha= input("escolha cara ou coroa").lower()
+aleatorio= random.choice(["cara","coroa"])
+if escolha== aleatorio:
+     print ('parabens você ganhou')
+else:
+     print("você perdeu hahahahaha")
+print(f"o certo seria {aleatorio}")
